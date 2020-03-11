d5c13be2dcbcc29d40c264aab863e69729627d92
Cenk Cetinkaya <mail@cenkcetinkaya.com>
2020-03-11 12:13:35 +0300

    Update theme.ts

--- scripts/theme.ts
+++ scripts/theme.ts
@@ -1,22 +1,43 @@
-const Application = require("sf-core/application");
-const config = require("./settings.json").config;
+import Data = require("sf-core/data");
+import Application = require("sf-core/application");
+const { config } = require("./settings.json");
 const themeConfig = config.theme;
 const { createThemeContextBound } = require("@smartface/contx/lib/styling/ThemeContext");
-
+const currentTheme = Data.getStringVariable("currentTheme") || themeConfig.currentTheme;
+const { clearCache } = require("sf-extension-utils/lib/getCombinedStyle");
 const themeSources = themeConfig.themes
     .map(name => ({
         name,
         rawStyles: require(`./generated/themes/${name}`),
-        isDefault: themeConfig.currentTheme === name
+        isDefault: currentTheme === name
     }));
+Application["theme"] = createThemeContextBound(themeSources);
+type ThemeListener = (themeName: string) => void;
 
-Application.theme = createThemeContextBound(themeSources);
-
+const themeListeners = new WeakMap<{}, ThemeListener>();
+const themeListenerKeys:{}[] = [];
 export const ThemeService = {
+    onChange(listener: ThemeListener) {
+        const key = {};
+        themeListenerKeys.push(key)
+        themeListeners.set(key, listener);
+        const deletionIndex = themeListenerKeys.length - 1;
+
+        return () => {
+            themeListeners.delete(listener);
+            themeListenerKeys.splice(deletionIndex, 1);
+        }
+    },
     changeTheme(name: string) {
-        Application.theme()({
+        Application["theme"]()({
             type: "changeTheme",
-            themeName: name
+            theme: name
+        });
+        clearCache();
+        themeListenerKeys.forEach((key) => {
+            if(themeListeners.has(key)){
+                themeListeners.get(key)(name);
+            }
         })
     }
 }
