0a94614f62171752ddb4b3057d6a60e8e49ef8f2
cenkce <cenk.cetinkaya@smartface.io>
2020-03-16 22:22:40 +0000

    Improvements

--- scripts/pages/page1.ts
+++ scripts/pages/page1.ts
@@ -4,16 +4,17 @@ import PageTitleLayout  from "components/PageTitleLayout";
 import System = require("sf-core/device/system");
 
 export default class Page1 extends Page1Design {
+    router: any;
 	constructor () {
         super();
 		// Overrides super.onShow method
-		this.onShow = onShow.bind(this, this.onShow.bind(this));
+        this.onShow = onShow.bind(this, this.onShow.bind(this));
 		// Overrides super.onLoad method
 		this.onLoad = onLoad.bind(this, this.onLoad.bind(this));
         this.btnNext.onPress = () => {
-            this['router'].push("/pages/page2", { message: "Hello World!" });
+            this.router.push("/pages/page2", { message: "Hello World!" });
         }
-	}
+    }
 }
 
 /**
