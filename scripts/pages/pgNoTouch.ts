7d9d62a6766be70c71fd128ba600825d954f66a5
furkan.arabaci <furkan.arabaci@smartface.io>
2021-05-25 16:46:21 +0000

    add notouch page

--- scripts/pages/pgNoTouch.ts
+++ scripts/pages/pgNoTouch.ts
@@ -1,30 +1,59 @@
 import PgNoTouchDesign from 'generated/pages/pgNoTouch';
+import System from 'sf-core/device/system';
 
 export default class PgNoTouch extends PgNoTouchDesign {
-	constructor() {
-		super();
-		// Overrides super.onShow method
-		this.onShow = onShow.bind(this, this.onShow.bind(this));
-		// Overrides super.onLoad method
-		this.onLoad = onLoad.bind(this, this.onLoad.bind(this));
-	}
+    isPageTouchEnabled = true;
+    touchCounter = 0;
+    constructor() {
+        super();
+        this.onShow = onShow.bind(this, this.onShow.bind(this));
+        this.onLoad = onLoad.bind(this, this.onLoad.bind(this));
+    }
+
+    async yourServiceCall(): Promise<void> {
+        return new Promise((resolve, reject) => {
+            setTimeout(() => resolve(), 3000);
+        })
+    }
+
+    initButtons() {
+        this.btnTimeout.onPress = async () => {
+            this.togglePageTouch(false);
+            await this.yourServiceCall();
+            this.togglePageTouch(true);
+        }
+        this.btnCounter.onPress = () => {
+            this.touchCounter++;
+            this.btnCounter.text = `Touch Counter : ${this.touchCounter}`;
+        }
+    }
+
+    togglePageTouch(touchEnabled: boolean) {
+        this.isPageTouchEnabled = touchEnabled;
+        this.btnTimeout.enabled = this.isPageTouchEnabled;
+        this.lblTouchIndicator.text = `Current Touch Status : ${this.isPageTouchEnabled ? 'Enabled' : 'Disabled'}`;
+        if (System.OS === System.OSType.IOS) {
+            this.layout.touchEnabled = this.isPageTouchEnabled;
+        }
+        else {
+            /**
+             * If you need to use this event somewhere else, add the code snippet below.
+             */
+            this.layout.android.onInterceptTouchEvent = () => {
+                if (this.isPageTouchEnabled === false) {
+                    return !this.isPageTouchEnabled;
+                }
+                // ...your other code
+            }
+        }
+    }
 }
 
-/**
- * @event onShow
- * This event is called when a page appears on the screen (everytime).
- * @param {function} superOnShow super onShow function
- * @param {Object} parameters passed from Router.go function
- */
 function onShow(superOnShow: () => void) {
-	superOnShow();
+    superOnShow();
 }
 
-/**
- * @event onLoad
- * This event is called once when page is created.
- * @param {function} superOnLoad super onLoad function
- */
 function onLoad(superOnLoad: () => void) {
-	superOnLoad();
+    superOnLoad();
+    this.initButtons();
 }
