45af548e1e61373421f41d5666262750bc4232de
furkan.arabaci <furkan.arabaci@smartface.io>
2021-06-04 15:04:35 +0000

    add spriteview

---
+++ scripts/pages/pgSpriteView.ts
@@ -0,0 +1,51 @@
+import PgSpriteViewDesign from 'generated/pages/pgSpriteView';
+import SpriteView from '@smartface/extension-spriteview';
+import Image from 'sf-core/ui/image';
+import ImageView from 'sf-core/ui/imageview';
+
+export default class PgSpriteView extends PgSpriteViewDesign {
+    spriteView: SpriteView;
+    constructor() {
+        super();
+        // Overrides super.onShow method
+        this.onShow = onShow.bind(this, this.onShow.bind(this));
+        // Overrides super.onLoad method
+        this.onLoad = onLoad.bind(this, this.onLoad.bind(this));
+    }
+    initSpriteView() {
+        this.spriteView = new SpriteView({
+            width: 100, height: 125,
+            imageFillType: ImageView.FillType.ASPECTFIT
+        });
+        this.spriteView.setSprite({
+            sheet: Image.createFromFile("images://braid.png"), // Image for the frame
+            frameX: 7, // Distinct image count on X axis
+            frameY: 4, // Distinct image count on Y axis
+            frameCount: 27, // Frame count of the image
+        });
+        this.layout.addChild(this.spriteView);
+        this.layout.applyLayout();
+
+    }
+}
+
+/**
+ * @event onShow
+ * This event is called when a page appears on the screen (everytime).
+ * @param {function} superOnShow super onShow function
+ * @param {Object} parameters passed from Router.go function
+ */
+function onShow(superOnShow: () => void) {
+    superOnShow();
+    this.spriteView.play(500);
+}
+
+/**
+ * @event onLoad
+ * This event is called once when page is created.
+ * @param {function} superOnLoad super onLoad function
+ */
+function onLoad(superOnLoad: () => void) {
+    superOnLoad();
+    this.initSpriteView();
+}
