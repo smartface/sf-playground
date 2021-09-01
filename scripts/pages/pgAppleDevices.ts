c5b51bb51e6a862ab578ec9c2b11a3840bcd6b6a
Cihat Yeşildağ <cihat.yesildag@smartface.io>
2021-09-01 20:16:52 +0300

    Added new page pgAppleDevices for testing copy, orientation, appleDevices modules

---
+++ scripts/pages/pgAppleDevices.ts
@@ -0,0 +1,67 @@
+import PgAppleDevicesDesign from 'generated/pages/pgAppleDevices';
+import { getModelName } from "@smartface/extension-utils/lib/appleDevices";
+import LviPages from 'components/LviPages';
+import deviceMappings from "@smartface/extension-utils/lib/appleDevices/deviceMapping.json";
+import copy from "@smartface/extension-utils/lib/copy";
+import orientationLib from "@smartface/extension-utils/lib/orientation";
+
+export default class PgAppleDevices extends PgAppleDevicesDesign {
+    dataSet: string[];
+    constructor() {
+        super();
+        // Overrides super.onShow method
+        this.onShow = onShow.bind(this, this.onShow.bind(this));
+        // Overrides super.onLoad method
+        this.onLoad = onLoad.bind(this, this.onLoad.bind(this));
+
+        this.onOrientationChange = () => {
+            const orientation = orientationLib.getOrientationOnchage();
+            this.onOrientationChangeForDataset();
+        }
+    }
+
+    onOrientationChangeForDataset() {
+        this.dataSet.sort(() => .5 - Math.random());
+        this.refreshListView();
+    }
+
+    initCopyDeviceMapping() {
+        const copiedJSONFile = copy(deviceMappings);
+        this.dataSet = Object.values(copiedJSONFile);
+    }
+    initListView() {
+        this.lvAppleDevices.refreshEnabled = false;
+        this.lvAppleDevices.rowHeight = 54;
+        this.lvAppleDevices.onRowBind = (listViewItem: LviPages, index) => {
+            listViewItem.lblPageName.text = this.dataSet[index];
+        }
+    }
+
+    refreshListView() {
+        this.lvAppleDevices.itemCount = this.dataSet.length;
+        this.lvAppleDevices.refreshData();
+    }
+}
+
+/**
+ * @event onShow
+ * This event is called when a page appears on the screen (everytime).
+ * @param {function} superOnShow super onShow function
+ * @param {Object} parameters passed from Router.go function
+ */
+function onShow(superOnShow: () => void) {
+    superOnShow();
+    this.headerBar.title = getModelName();
+    this.refreshListView();
+}
+
+/**
+ * @event onLoad
+ * This event is called once when page is created.
+ * @param {function} superOnLoad super onLoad function
+ */
+function onLoad(superOnLoad: () => void) {
+    superOnLoad();
+    this.initCopyDeviceMapping();
+    this.initListView();
+}
