cdaf7d0882966ad82b14478c4ec7d5dc65d46a49
Furkan Arabacı <furkan.arabaci@smartface.io>
2022-02-07 18:00:44 +0300

    remove sourcemap file and add it from module

--- scripts/app.ts
+++ scripts/app.ts
@@ -1,17 +1,18 @@
 /* globals lang */
 import "i18n/i18n"; // Generates global lang object
 import Application from "@smartface/native/application";
-import { errorStackBySourceMap } from "error-by-sourcemap";
+import { errorStackBySourceMap } from "@smartface/source-map";
 import System from "@smartface/native/device/system";
 // Set uncaught exception handler, all exceptions that are not caught will
 // trigger onUnhandledError callback.
 Application.on(Application.Events.UnhandledError, (e: UnhandledError) => {
-  console.log("Application.Events.UnhandledError");
   const error = errorStackBySourceMap(e);
-  alert({
-    title: e.type || lang.applicationError,
-    message: System.OS === "Android" ? error.stack : e.message + "\n\n*" + error.stack,
-  });
+  const message = {
+    message: System.OS === System.OSType.ANDROID ? error.stack : e.message,
+    stack: System.OS === System.OSType.IOS ? error.stack : undefined
+  };
+  console.error(message);
+  alert(JSON.stringify(message, null, 2), e.type || lang.applicationError);
 });
 
 import "start";
