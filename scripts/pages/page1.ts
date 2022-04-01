51ffd672a6cfdbdc4dab56b60a98c48342c02590
Cihat Yeşildağ <cico643@hotmail.com.tr>
2022-04-01 13:43:03 +0300

    add videoview example

--- scripts/pages/page1.ts
+++ scripts/pages/page1.ts
@@ -2,6 +2,7 @@ import Page1Design from "generated/pages/page1";
 import Router from "@smartface/router/lib/router/Router";
 import { Route } from "@smartface/router";
 import { withDismissAndBackButton } from "@smartface/mixins";
+import File from "@smartface/native/io/file";
 
 export default class Page1 extends withDismissAndBackButton(Page1Design) {
   constructor(private router?: Router, private route?: Route) {
@@ -14,6 +15,10 @@ export default class Page1 extends withDismissAndBackButton(Page1Design) {
   onShow() {
     super.onShow();
     this.initBackButton(this.router);
+    const myFile = new File({
+        path: 'assets://test.mp4'
+    });
+    this.videoView1.loadFile(myFile);
   }
 
   /**
