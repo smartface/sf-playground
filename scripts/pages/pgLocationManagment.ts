a1071f81ae3eb74cdb4c50788973ad97f279b805
alimoncul <alimoncul@gmail.com>
2022-04-25 10:42:17 +0300

    feature: add last known location

--- scripts/pages/pgLocationManagment.ts
+++ scripts/pages/pgLocationManagment.ts
@@ -7,84 +7,93 @@ import { openApplicationSettings } from "@smartface/extension-utils/lib/settings
 import { Route } from "@smartface/router";
 import { withDismissAndBackButton } from "@smartface/mixins";
 import { Router } from "@smartface/router";
+import Location from "@smartface/native/device/location";
+import { ButtonEvents } from "@smartface/native/ui/button/button-events";
 
 const CURRENT_LOCATION = "Current Location";
-type Location = { latitude: number; longitude: number };
+type LocationType = { latitude: number; longitude: number; };
 
 /**
  * Testing location, maps, navigation, settings and locaiton permission
  */
 
 export default class PgLocationManagment extends withDismissAndBackButton(PgLocationManagmentDesign) {
-  location: Location;
-  constructor(private router?: Router, private route?: Route) {
-    super({});
-  }
+    location: LocationType;
+    constructor(private router?: Router, private route?: Route) {
+        super({});
+    }
 
-  initButtons() {
-    this.btnGetLocation.onPress = async () => await this.getLocation();
-    this.btnNavigate.onPress = () => this.navigateToLocation(this.location);
-    this.btnNavigate.enabled = false;
-    this.btnOpenApplicationSettings.onPress = () => openApplicationSettings();
-  }
+    initButtons() {
+        this.btnGetLocation.onPress = async () => await this.getLocation();
+        this.btnNavigate.onPress = () => this.navigateToLocation(this.location);
+        this.btnLastKnownLocation.on(ButtonEvents.Press, () => this.getLastKnownLocation());
+        this.btnNavigate.enabled = false;
+        this.btnOpenApplicationSettings.onPress = () => openApplicationSettings();
+    }
 
-  initLabels() {
-    this.lblLatitude.text = "";
-    this.lblLongitude.text = "";
-  }
+    initLabels() {
+        this.lblLatitude.text = "";
+        this.lblLongitude.text = "";
+    }
 
-  async getLocation() {
-    this.location = await location.getLocation();
-    const _location = this.location;
-    this.setCoordinateText(_location);
-    this.markLocation(_location);
-    this.btnNavigate.enabled = true;
-    alert("Touch the pin!");
-  }
+    async getLocation() {
+        this.location = await location.getLocation();
+        const _location = this.location;
+        this.setCoordinateText(_location);
+        this.markLocation(_location);
+        this.btnNavigate.enabled = true;
+        alert("Touch the pin!");
+    }
 
-  setCoordinateText(coordinate: Location) {
-    const { latitude, longitude } = coordinate;
-    this.lblLatitude.text = latitude.toFixed(10);
-    this.lblLongitude.text = longitude.toFixed(10);
-  }
+    setCoordinateText(coordinate: LocationType) {
+        const { latitude, longitude } = coordinate;
+        this.lblLatitude.text = latitude.toFixed(10);
+        this.lblLongitude.text = longitude.toFixed(10);
+    }
 
-  markLocation(location: Location) {
-    this.mapView1.removeAllPins();
-    const params = { location, title: CURRENT_LOCATION };
-    const currentLocationPin = new MapView.Pin(params);
-    currentLocationPin.onPress = () => this._showMapsMenu(location);
-    this.mapView1.addPin(currentLocationPin);
-    this.mapView1.setCenterLocationWithZoomLevel(location, 12, true);
-  }
+    markLocation(location: LocationType) {
+        this.mapView1.removeAllPins();
+        const params = { location, title: CURRENT_LOCATION };
+        const currentLocationPin = new MapView.Pin(params);
+        currentLocationPin.onPress = () => this._showMapsMenu(location);
+        this.mapView1.addPin(currentLocationPin);
+        this.mapView1.setCenterLocationWithZoomLevel(location, 12, true);
+    }
 
-  _showMapsMenu(location: Location) {
-    const page = this;
-    const options = {
-      page,
-      location,
-      name: CURRENT_LOCATION,
-    };
-    showMapsMenu(options);
-  }
+    getLastKnownLocation() {
+        Location.getLastKnownLocation((e) => {
+            alert('Last location: ' + e.latitude + ' ' + e.longitude);
+        }, () => alert('Could not get last known location'));
+    }
 
-  navigateToLocation(location: Location) {
-    const page = this;
-    const options = {
-      page,
-      location,
-      transportType: "d",
-    };
-    showNavigationMenu(options);
-  }
+    _showMapsMenu(location: LocationType) {
+        const page = this;
+        const options = {
+            page,
+            location,
+            name: CURRENT_LOCATION,
+        };
+        showMapsMenu(options);
+    }
 
-  onShow() {
-    super.onShow();
-    this.initBackButton(this.router);
-  }
+    navigateToLocation(location: LocationType) {
+        const page = this;
+        const options = {
+            page,
+            location,
+            transportType: "d",
+        };
+        showNavigationMenu(options);
+    }
 
-  onLoad() {
-    super.onLoad();
-    this.initButtons();
-    this.initLabels();
-  }
+    onShow() {
+        super.onShow();
+        this.initBackButton(this.router);
+    }
+
+    onLoad() {
+        super.onLoad();
+        this.initButtons();
+        this.initLabels();
+    }
 }
