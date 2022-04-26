216e4ddcf461aa1a561b5938c369ec1c106550e3
alimoncul <alimoncul@gmail.com>
2022-04-26 15:11:41 +0300

    tooltip

--- scripts/start.ts
+++ scripts/start.ts
@@ -2,3 +2,6 @@ import 'theme';
 import router from 'routes';
 
 router.push('/root/btb/tab0/mainpage');
+
+//You can update this path to open directly the page you want.
+//For example /root/btb/tab0/pgHeaderImageAndItems
