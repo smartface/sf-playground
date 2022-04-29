175e9626e8a70cc0a49c6c0df0d5f27d98a748a8
alimoncul <alimoncul@gmail.com>
2022-04-29 16:11:13 +0300

    refactor: sfl

--- scripts/pages/pgShimmerFlexLayout.ts
+++ scripts/pages/pgShimmerFlexLayout.ts
@@ -1,50 +1,37 @@
 import PgShimmerFlexLayoutDesign from 'generated/pages/pgShimmerFlexLayout';
 import { withDismissAndBackButton } from '@smartface/mixins';
 import { Router, Route } from '@smartface/router';
-import ShimmerFlexLayout from '@smartface/native/ui/shimmerflexlayout';
 import Button from '@smartface/native/ui/button';
 
 export default class PgShimmerFlexLayout extends withDismissAndBackButton(PgShimmerFlexLayoutDesign) {
-  shimmerLayout: ShimmerFlexLayout;
-  btnToggleShimmer: Button;
   constructor(private router?: Router, private route?: Route) {
     super({});
-    this.shimmerLayout = this.children['shimmerFlexLayout1'];
-    this.btnToggleShimmer = this.children['btnToggleShimmer'];
   }
 
-  /**
-   * @event onShow
-   * This event is called when the page appears on the screen (everytime).
-   */
-  onShow() {
-    super.onShow();
-    this.initBackButton(this.router); //Addes a back button to the page headerbar.
-  }
   initShimmerFlexLayout() {
-    this.shimmerLayout.startShimmering();
+    this.sfl.startShimmering();
   }
+
   initButton() {
     this.btnToggleShimmer.on('press', () => {
-      if (this.shimmerLayout.isShimmering) {
-        this.shimmerLayout.stopShimmering();
+      if (this.sfl.isShimmering || false) {
+        this.sfl.stopShimmering();
         this.btnToggleShimmer.text = 'Start Shimmering';
       } else {
-        this.shimmerLayout.startShimmering();
+        this.sfl.startShimmering();
         this.btnToggleShimmer.text = 'Stop Shimmering';
       }
     });
   }
 
-  /**
-   * @event onLoad
-   * This event is called once when the page is created.
-   */
+  onShow() {
+    super.onShow();
+    this.initBackButton(this.router); //Addes a back button to the page headerbar.
+  }
+
   onLoad() {
     super.onLoad();
-    this.btnToggleShimmer.onPress = () => {
-      console.info(this.router.canGoBack());
-      // this.router.pushAndBack()
-    };
+    this.initButton();
+    this.initShimmerFlexLayout();
   }
 }
