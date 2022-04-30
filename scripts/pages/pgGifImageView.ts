94725068dd38e8691a5c646ac1e6f8b108fa5ecc
alimoncul <alimoncul@gmail.com>
2022-04-30 22:19:15 +0300

    feature: gifimageview

--- scripts/pages/pgGifImageView.ts
+++ scripts/pages/pgGifImageView.ts
@@ -1,34 +1,44 @@
 import PgGifImageViewDesign from 'generated/pages/pgGifImageView';
 import { withDismissAndBackButton } from '@smartface/mixins';
 import { Router, Route } from '@smartface/router';
-import File from '@smartface/native/io/file';
-import Path from '@smartface/native/io/path';
 import GifImage from '@smartface/native/ui/gifimage';
 
 export default class PgGifImageView extends withDismissAndBackButton(PgGifImageViewDesign) {
+  private _interval: NodeJS.Timer;
   constructor(private router?: Router, private route?: Route) {
     super({});
+    this.btnStart.on('press', () => this.giv.startAnimating());
+    this.btnStop.on('press', () => this.giv.stopAnimating());
   }
 
   initGif() {
-    // this.giv.gifImage = GifImage.createFromFile('assets://countdown.gif');
-    this.giv.loadFromFile({ file: new File({ path: Path.AssetsUriScheme + 'countdown.gif' }) });
+    //@ts-ignore
+    this.giv.ios.loopCompletionCallback = () => console.info('loopCompletionCallback');
+    this.giv.gifImage = GifImage.createFromFile('assets://countdown.gif');
   }
-  /**
-   * @event onShow
-   * This event is called when the page appears on the screen (everytime).
-   */
+
+  getInfo() {
+    setInterval(() => {
+      console.info({
+        currentFrame: this.giv.currentFrame,
+        currentFrameIndex: this.giv.currentFrameIndex,
+        isAnimating: this.giv.isAnimating
+      });
+    }, 2000);
+  }
+
+  onHide() {
+    clearInterval(this._interval);
+  }
+
   onShow() {
     super.onShow();
     this.initBackButton(this.router); //Addes a back button to the page headerbar.
     this.initGif();
   }
 
-  /**
-   * @event onLoad
-   * This event is called once when the page is created.
-   */
   onLoad() {
     super.onLoad();
+    this.getInfo();
   }
 }
