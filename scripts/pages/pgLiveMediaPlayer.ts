8760770ef11ae9119f771a14ee52f3899fc38ffc
Furkan Arabacı <furkan.arabaci@smartface.io>
2022-05-07 22:39:33 +0300

    improve livemediaplayer

--- scripts/pages/pgLiveMediaPlayer.ts
+++ scripts/pages/pgLiveMediaPlayer.ts
@@ -10,8 +10,31 @@ class StyleableLiveMediaPlayer extends styleableComponentMixin(LiveMediaPlayer) 
 
 export default class PgLiveMediaPlayer extends withDismissAndBackButton(PgLiveMediaPlayerDesign) {
   private liveMediaPlayer: StyleableLiveMediaPlayer;
+  private isPlaying = false;
   constructor(private router?: Router, private route?: Route) {
     super({});
+    this.btnAction.on('press', () => {
+      if (this.isPlaying) {
+        this.liveMediaPlayer?.stop();
+        this.btnAction.text = 'Start';
+        this.isPlaying = false;
+      } else {
+        this.liveMediaPlayer?.start();
+        this.btnAction.text = 'Stop';
+        this.isPlaying = true;
+      }
+    });
+    this.btnPause.on('press', () => {
+      if (this.isPlaying) {
+        this.liveMediaPlayer?.pause();
+        this.btnPause.text = 'Resume';
+        this.isPlaying = false;
+      } else {
+        this.liveMediaPlayer?.start();
+        this.btnPause.text = 'Pause';
+        this.isPlaying = true;
+      }
+    });
   }
 
   initLiveMediaPlayer() {
@@ -20,19 +43,11 @@ export default class PgLiveMediaPlayer extends withDismissAndBackButton(PgLiveMe
       inputUrl: 'https://tv-trt1.medya.trt.com.tr/master_720.m3u8',
       backgroundColor: Color.BLACK,
       onChange: (params) => {
-        console.info(params);
+        console.info(params.message, params);
       },
       scaleType: ScaleType.ASPECTFILL
     });
     this.flLiveMediaPlayer.addChild(this.liveMediaPlayer, 'liveMediaPlayer', '.grow-relative');
-  }
-
-  startLiveMediaPlayer() {
-    this.liveMediaPlayer?.start();
-  }
-
-  stopLiveMediaPlayer() {
-    this.liveMediaPlayer?.stop();
   }
   /**
    * @event onShow
@@ -41,7 +56,6 @@ export default class PgLiveMediaPlayer extends withDismissAndBackButton(PgLiveMe
   onShow() {
     super.onShow();
     this.initBackButton(this.router); //Addes a back button to the page headerbar.
-    this.startLiveMediaPlayer();
   }
 
   /**
