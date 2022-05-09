ce490ea501424bcd7b6925359c26bb5fa0faf0e2
alimoncul <alimoncul@gmail.com>
2022-05-09 11:59:09 +0300

    feature: add missing props for tbc

--- scripts/pages/pgTabbarController.ts
+++ scripts/pages/pgTabbarController.ts
@@ -5,34 +5,39 @@ import PgLabel from 'pages/pgLabel';
 import PgCrypto from 'pages/pgCrypto';
 import TabbarItem from '@smartface/native/ui/tabbaritem';
 import Color from '@smartface/native/ui/color';
+import OverScrollMode from '@smartface/native/ui/shared/android/overscrollmode';
 
 export default class PgTabbarController extends TabBarController {
-  pages = [PgPicker, PgLabel, PgCrypto];
+  pages = [PgPicker, PgLabel, PgCrypto, PgPicker, PgLabel, PgCrypto];
   items = this.pages.map((page) => new TabbarItem({ title: page.name, icon: 'images://arrowbottom.png' }));
   constructor(private router?: Router, private route?: Route) {
     super({});
+  }
+
+  onSelected(index: number) {
+    console.info('onSelected: ', index);
   }
 
   onPageCreate(index: number) {
     return new this.pages[index](this.router, this.route);
   }
 
-  /**
-   * @event onShow
-   * This event is called when the page appears on the screen (everytime).
-   */
   onShow() {
     super.onShow();
+    // setTimeout(() => this.setSelectedIndex(3, false), 2000);
   }
 
-  /**
-   * @event onLoad
-   * This event is called once when the page is created.
-   */
   onLoad() {
     super.onLoad();
+    this.barHeight = 22;
+    this.android.dividerColor = Color.RED;
+    this.android.dividerPadding = 2;
+    this.android.dividerWidth = 11;
+    this.android.overScrollMode = OverScrollMode.ALWAYS;
+    this.pagingEnabled = true;
+    this.scrollEnabled = false;
+    this.autoCapitalize = false;
     this.headerBar.title = this.constructor.name; //Tabbarcontroller requires manual setup
-    this.scrollEnabled = true;
     this.indicatorColor = Color.BLACK;
     this.indicatorHeight = 3;
     this.barColor = Color.create('#F3F0F0');
@@ -40,6 +45,5 @@ export default class PgTabbarController extends TabBarController {
       normal: Color.BLACK,
       selected: Color.create('#00A1F1')
     };
-    this.autoCapitalize = true;
   }
 }
