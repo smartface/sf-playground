461ec1d1f8a012dcce53989e2b50ec061b4d9abb
alimoncul <alimoncul@gmail.com>
2022-05-09 10:21:23 +0300

    fix: timepicker

--- scripts/pages/pgTimePicker.ts
+++ scripts/pages/pgTimePicker.ts
@@ -4,31 +4,28 @@ import { Router, Route } from '@smartface/router';
 import TimePicker from '@smartface/native/ui/timepicker';
 
 export default class PgTimePicker extends withDismissAndBackButton(PgTimePickerDesign) {
+  private timePicker: TimePicker;
   constructor(private router?: Router, private route?: Route) {
     super({});
+    this.btnShowPicker.on('press', () => this.timePicker.show());
   }
 
-  /**
-   * @event onShow
-   * This event is called when the page appears on the screen (everytime).
-   */
+  initPicker() {
+    this.timePicker = new TimePicker();
+    this.timePicker.on('selected', (time) => {
+      console.log('Hour: ' + time.hour + ' Minute: ' + time.minute);
+    });
+    this.timePicker.is24HourFormat = true;
+    this.timePicker.setTime({ hour: new Date().getHours(), minute: new Date().getMinutes() });
+  }
+
   onShow() {
     super.onShow();
+    this.initPicker();
     this.initBackButton(this.router); //Addes a back button to the page headerbar.
   }
 
-  /**
-   * @event onLoad
-   * This event is called once when the page is created.
-   */
   onLoad() {
     super.onLoad();
-    const myTimePicker = new TimePicker();
-    myTimePicker.on('selected', (time) => {
-      console.log('Hour: ' + time.hour + ' Minute: ' + time.minute);
-    });
-
-    myTimePicker.is24HourFormat = true;
-    myTimePicker.show();
   }
 }
