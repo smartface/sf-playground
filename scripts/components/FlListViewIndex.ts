d170b8b2044de3f246f42b7080727649ff161c5f
alimoncul <alimoncul@gmail.com>
2022-05-12 17:44:27 +0300

    feature: listviewindex component

---
+++ scripts/components/FlListViewIndex.ts
@@ -0,0 +1,65 @@
+import FlListViewIndexDesign from 'generated/my-components/FlListViewIndex';
+import Screen from '@smartface/native/device/screen';
+import Label from '@smartface/native/ui/label';
+import { themeService } from 'theme';
+const labelLayoutStyles = themeService.getNativeStyle('.flListViewIndex-label');
+const labelStyles = themeService.getNativeStyle('.flListViewIndex-label');
+const labelHeight = themeService.getNativeStyle('.flListViewIndex-label').height;
+
+const FL_LIST_VIEW_INDEX_LOCK_TIMEOUT = 200;
+
+export default class FlListViewIndex extends FlListViewIndexDesign {
+  pageName?: string | undefined;
+  lock: boolean = false;
+  labels: Label[] = [];
+  _values: string[] = [];
+  _topMargin: number = 0;
+  _onItemSelect: (selectedLabel: string) => void;
+  constructor(props?: any, pageName?: string) {
+    super(props);
+    this.pageName = pageName;
+    this.on('touchMoved', (e) => {
+      if (this._onItemSelect) {
+        const selectedLabel = this.calculateItem(e.y);
+        !this.lock && this._onItemSelect(selectedLabel);
+        this.lock = true;
+        setTimeout(() => (this.lock = false), FL_LIST_VIEW_INDEX_LOCK_TIMEOUT);
+      }
+    });
+    this.on('touchEnded', (e) => {
+      if (this._onItemSelect) {
+        const selectedLabel = this.calculateItem(e.y);
+        !this.lock && this._onItemSelect(selectedLabel);
+        this.lock = true;
+        setTimeout(() => (this.lock = false), FL_LIST_VIEW_INDEX_LOCK_TIMEOUT);
+      }
+    });
+  }
+  calculateItem(y: number) {
+    return this._values[Math.round(y / labelHeight)] || this._values[this._values.length - 1];
+  }
+  set onItemSelect(value: (selectedLabel: string) => void) {
+    this._onItemSelect = value;
+  }
+  set topMargin(value: number) {
+    this._topMargin = value;
+  }
+  get topMargin(): number {
+    return this._topMargin;
+  }
+  topToDispatch(): number {
+    const height = (this._values || []).length * labelHeight;
+    return (Screen.height - height) / 2;
+  }
+  set indexes(values: string[]) {
+    this._values = values;
+    values.forEach((value, index) => {
+      const label = new Label({ ...labelLayoutStyles, ...labelStyles });
+      label.text = value;
+      //@ts-ignore
+      this.addChild(label, `${value}/${index}`);
+      label.applyLayout();
+      this.labels.push(label);
+    });
+  }
+}
--- scripts/pages/mainPage.ts
+++ scripts/pages/mainPage.ts
@@ -6,9 +6,12 @@ import { withDismissAndBackButton } from '@smartface/mixins';
 import Page from '@smartface/native/ui/page';
 import { ConstructorOf } from '@smartface/styling-context/lib/ConstructorOf';
 import Application from '@smartface/native/application';
+import System from '@smartface/native/device/system';
 
 export default class MainPage extends withDismissAndBackButton(MainPageDesign) {
   _pages: ConstructorOf<Page>[] = [];
+  _searchStrings: string[] = [];
+  _searchKeys: string[] = [];
   constructor(private router?: Router, private route?: Route, params?: any) {
     super({});
     this.lblDisclaimer.on('touchEnded', () => {
@@ -17,13 +20,31 @@ export default class MainPage extends withDismissAndBackButton(MainPageDesign) {
     });
   }
 
+  initListViewIndex() {
+    const statusBarHeight = System.OS === System.OSType.ANDROID ? Application.statusBar.height : 0;
+    this.flListViewIndex.topMargin = System.OSType.ANDROID ? this.headerBar.height : 40 + statusBarHeight;
+    this.flListViewIndex.indexes = this._searchKeys;
+    this.flListViewIndex.onItemSelect = (selectedLabel: string) => {
+      const indexToScroll = this._searchStrings.findIndex((ss) => ss[0] === selectedLabel);
+      if (indexToScroll > -1) {
+        this.lvPages.scrollTo(indexToScroll, false);
+      }
+    };
+    this.flListViewIndex.top = this.flListViewIndex.topToDispatch() - this.flListViewIndex.topMargin;
+    // TODO: dispatch crashes this
+    // this.flListViewIndex.dispatch({
+    //   type: 'updateUserStyle',
+    //   userStyle: {
+    //     top: this.flListViewIndex.topToDispatch() - this.flListViewIndex.topMargin
+    //   }
+    // });
+  }
+
   initListView() {
     this.lvPages.refreshEnabled = false;
     this.lvPages.rowHeight = 54;
     this.lvPages.onRowBind = (listViewItem: LviPages, index) => {
-      const name = this.pages[index].name;
-      const textWithoutPrefix = name.substring(0, 2) === 'Pg' ? name.slice(2) : name;
-      listViewItem.lblPageName.text = textWithoutPrefix;
+      listViewItem.lblPageName.text = this._searchStrings[index];
     };
     this.lvPages.onRowSelected = (listViewItem: LviPages, index) => {
       this.router.push(`${this.pages[index].name}`);
@@ -32,11 +53,14 @@ export default class MainPage extends withDismissAndBackButton(MainPageDesign) {
 
   refreshListView() {
     this.lvPages.itemCount = this.pages.length;
+    this.initListViewIndex();
     this.lvPages.refreshData();
   }
+
   onShow() {
     super.onShow();
   }
+
   onLoad() {
     super.onLoad();
     this.headerBar.leftItemEnabled = false;
@@ -44,6 +68,8 @@ export default class MainPage extends withDismissAndBackButton(MainPageDesign) {
   }
 
   set pages(value) {
+    this._searchKeys = [];
+    this._searchStrings = [];
     this._pages = value.sort((a, b) => {
       if (a.name < b.name) {
         return -1;
@@ -51,6 +77,15 @@ export default class MainPage extends withDismissAndBackButton(MainPageDesign) {
         return 1;
       }
     });
+    this._pages.forEach((a) => {
+      const textWithoutPrefix = a.name.substring(0, 2) === 'Pg' ? a.name.slice(2) : a.name;
+      this._searchStrings.push(textWithoutPrefix);
+      const firstCharacter = textWithoutPrefix.slice(0, 1);
+      if (!this._searchKeys.includes(firstCharacter)) {
+        this._searchKeys.push(firstCharacter);
+      }
+    });
+    this._searchKeys = this._searchKeys.sort();
     this.refreshListView();
   }
 
