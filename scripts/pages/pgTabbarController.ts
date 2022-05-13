eea1236611728f62031d9a6e5d14d71b0137ac63
alimoncul <alimoncul@gmail.com>
2022-05-13 20:01:51 +0300

    move to onshow

--- scripts/pages/pgTabbarController.ts
+++ scripts/pages/pgTabbarController.ts
@@ -1,14 +1,12 @@
 import { Router, Route } from '@smartface/router';
 import TabBarController from '@smartface/native/ui/tabbarcontroller';
-import PgPicker from 'pages/pgPicker';
 import PgLabel from 'pages/pgLabel';
-import PgCrypto from 'pages/pgCrypto';
 import TabbarItem from '@smartface/native/ui/tabbaritem';
 import Color from '@smartface/native/ui/color';
 import OverScrollMode from '@smartface/native/ui/shared/android/overscrollmode';
 
 export default class PgTabbarController extends TabBarController {
-  pages = [PgPicker, PgLabel, PgCrypto, PgPicker, PgLabel, PgCrypto];
+  pages = [PgLabel, PgLabel, PgLabel, PgLabel, PgLabel, PgLabel];
   items = this.pages.map((page) => new TabbarItem({ title: page.name, icon: 'images://arrowbottom.png' }));
   constructor(private router?: Router, private route?: Route) {
     super({});
@@ -22,13 +20,7 @@ export default class PgTabbarController extends TabBarController {
     return new this.pages[index](this.router, this.route);
   }
 
-  onShow() {
-    super.onShow();
-    // setTimeout(() => this.setSelectedIndex(3, false), 2000);
-  }
-
-  onLoad() {
-    super.onLoad();
+  updateStyles() {
     this.barHeight = 22;
     this.android.dividerColor = Color.RED;
     this.android.dividerPadding = 2;
@@ -37,7 +29,7 @@ export default class PgTabbarController extends TabBarController {
     this.pagingEnabled = true;
     this.scrollEnabled = false;
     this.autoCapitalize = false;
-    this.headerBar.title = this.constructor.name; //Tabbarcontroller requires manual setup
+    this.headerBar.title = 'Tabbar Controller'; //Tabbarcontroller requires manual setup
     this.indicatorColor = Color.BLACK;
     this.indicatorHeight = 3;
     this.barColor = Color.create('#F3F0F0');
@@ -45,5 +37,14 @@ export default class PgTabbarController extends TabBarController {
       normal: Color.BLACK,
       selected: Color.create('#00A1F1')
     };
+  }
+
+  onShow() {
+    super.onShow();
+    this.updateStyles();
+  }
+
+  onLoad() {
+    super.onLoad();
   }
 }
