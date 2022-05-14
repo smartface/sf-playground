934c2b8a1d373cb8ee3b572d52c5f46b2d596530
Furkan Arabacı <furkan.arabaci@smartface.io>
2022-05-14 12:07:36 +0300

    move zoomable to native

--- scripts/pages/pgZoomableImageView.ts
+++ scripts/pages/pgZoomableImageView.ts
@@ -1,23 +1,27 @@
 import PgZoomableImageViewDesign from 'generated/pages/pgZoomableImageView';
-import ZoomableImageView from '@smartface/extension-zoomable-imageview';
 import { withDismissAndBackButton } from '@smartface/mixins';
 import { Router, Route } from '@smartface/router';
+import ZoomableImageView from '@smartface/native/ui/zoomableimageview';
+import { IZoomableImageView } from '@smartface/native/ui/zoomableimageview/zoomableimageview';
+import { ImageFillType } from '@smartface/native/ui/imageview/imageview';
+import Color from '@smartface/native/ui/color';
 
 export default class PgZoomableImageView extends withDismissAndBackButton(PgZoomableImageViewDesign) {
-  zoomableImageView: InstanceType<typeof ZoomableImageView>;
+  zoomableImageView: IZoomableImageView;
   constructor(private router?: Router, private route?: Route) {
     super({});
   }
 
   initZoomableImageView() {
     this.zoomableImageView = new ZoomableImageView({
-      width: 250,
-      height: 250
+      flexGrow: 1
     });
     this.zoomableImageView.image = 'images://smartface.png';
     this.zoomableImageView.minimumZoomScale = 1;
+    this.zoomableImageView.imageFillType = ImageFillType.ASPECTFIT;
     this.zoomableImageView.android.mediumZoomScale = 2;
-    this.zoomableImageView.maximumZoomScale = 3;
+    this.zoomableImageView.maximumZoomScale = 5;
+    this.zoomableImageView.animated = true;
 
     this.addChild(this.zoomableImageView);
   }
