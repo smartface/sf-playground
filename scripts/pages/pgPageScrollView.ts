36807fb40c7f81a52a45a37912cf2de997425591
Guven Karanfil <guven.karanfil@smartface.io>
2022-06-13 15:58:18 +0300

    add flexlayout to make scrollable area bigger

--- scripts/pages/pgPageScrollView.ts
+++ scripts/pages/pgPageScrollView.ts
@@ -9,7 +9,6 @@ export default class PgPageScrollView extends withDismissAndBackButton(PgPageScr
         super({});
 
         this.button1.on('press', () => {
-
             this.label1.dispatch({
                 type: 'updateUserStyle',
                 userStyle: {
@@ -17,52 +16,8 @@ export default class PgPageScrollView extends withDismissAndBackButton(PgPageScr
                 }
             })
 
-            
-
-            // console.log('sc2 instance parent: ', this.scrollView2.getParent().constructor.name);
-            // console.log('sc2 layout instance parent: ', this.scrollView2.layout.getParent().constructor.name);
-
-            // console.log('sc2 layout instance second parent: ', this.scrollView2.layout.getParent().getParent().constructor.name);
-
-            // console.log('sc2 layout instance thrid parent: ', this.scrollView2.layout.getParent().getParent().getParent().constructor.name);
-
-            // console.log('sc2 layout instance thrid parent: ', this.scrollView2.layout.getParent().getParent().getParent().getParent().constructor.name);
-
-            // console.log('last element: ', this.scrollView2.layout.getParent().getParent().getParent().getParent().getParent());
-            // console.log('sc2 layout instance thrid parent: ', this.scrollView2.layout.getParent().getParent().getParent().getParent().getParent().constructor.name);
-
-
-
             this.scrollView2.dispatch({ 'type': 'updateUserStyle', userStyle: { height: 300 } })
-
-
             this.label1.textAlignment = TextAlignment.TOPLEFT
-
-            this.layout.applyLayout()
-
-            // console.log('is sc instance: ', this.scrollView1 instanceof ScrollView);
-
-
-            // @ts-ignore
-
-            // this.layout.nativeObject.setNeedsLayout()
-
-
-            /* SC > SC > Label TEST */
-            // this.scrollView2.layout.applyLayout()
-            // this.scrollView1.layout.applyLayout()
-
-
-            /* SC > Label TEST */
-            // this.label1.applyLayout()
-
-            // this.scrollView1.layout.applyLayout()
-
-            // this.scrollView1.height = 500
-            // this.layout.applyLayout()
-
-
-
         })
     }
 
